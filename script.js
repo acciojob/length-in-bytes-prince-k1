557a17d64164846b56061556fabeda3020a88ba1
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-30 10:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	let encoder = new TextEncoder();
+	let encoded = encoder.encode(str);
+	return encoded.length;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str))
